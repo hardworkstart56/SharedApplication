57ba0e17e221e9530c18938b5ca472cd13965167
AndroidProjects <payalvarade@gmail.com>
2019-05-14 14:45:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tatastrive/sharedapplication/Session.java
@@ -0,0 +1,35 @@
+package com.example.tatastrive.sharedapplication;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+class Session {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;   //SharedPreferance used to store data for small value not big value.It may be set or unset their value
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "snow-intro-slider";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public Session(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+
+}
